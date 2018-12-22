6b4d321535578ba2f0cb4b17e6ade1f469ca9d58
MLim619 <MLim619@gmail.com>
2018-12-21 19:27:20 -0800

    Refactor dto -> entity mapping to use AutoMapper

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -1,4 +1,5 @@
-﻿using Library.API.Helpers;
+﻿using AutoMapper;
+using Library.API.Helpers;
 using Library.API.Models;
 using Library.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -23,19 +24,8 @@ namespace Library.API.Controllers
         public IActionResult GetAuthors()
         {
             var authorsFromRepo = _libraryRepository.GetAuthors();
-            var authors = new List<AuthorDto>();
-
-            foreach (var author in authorsFromRepo)
-            {
-                authors.Add(new AuthorDto()
-                {
-                    Id = author.Id,
-                    Name = $"{author.FirstName} {author.LastName}",
-                    Genre = author.Genre,
-                    Age = author.DateOfBirth.GetCurrentAge()
-                });
-            }
-
+            var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+                       
             return new JsonResult(authors);
         }
     }
